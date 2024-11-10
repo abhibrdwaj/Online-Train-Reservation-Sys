b15a10664e797c9c11f3d9438059ac538bbc1f3e
Abhinav Bharadwaj <abhinavbharadwaj00@gmail.com>
2024-11-10 17:39:19 -0500

    SQL changes

--- sql/create-database.sql
+++ sql/create-database.sql
@@ -1,31 +1,28 @@
-CREATE DATABASE TrainReservation;
-Use TrainReservation;
+CREATE DATABASE RailwayBookingSystem;
+Use RailwayBookingSystem;
 
 CREATE TABLE Stations (
-    station_id INT,
+    station_id INT PRIMARY KEY,
     station_name VARCHAR(100) NOT NULL,
     city VARCHAR(100) NOT NULL,
-    state VARCHAR(100) NOT NULL,
-    PRIMARY KEY (station_id)
+    state VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE TransitLines (
-    name VARCHAR(100) UNIQUE,
-    origin_station_id INT,
-    destination_station_id INT,
+    name VARCHAR(100) PRIMARY KEY,
+    origin_station_id INT NOT NULL,
+    destination_station_id INT NOT NULL,
     fare DECIMAL(10, 2) NOT NULL,
     no_of_stops INT NOT NULL,
-    PRIMARY KEY (name),
     FOREIGN KEY (origin_station_id) REFERENCES Stations(station_id),
     FOREIGN KEY (destination_station_id) REFERENCES Stations(station_id)
 );
 
 CREATE TABLE Trains (
-    train_id INT CHECK (train_id >= 1000 AND train_id <= 9999),
+    train_id INT AUTO_INCREMENT PRIMARY KEY,
     transit_line_name VARCHAR(100),
-    PRIMARY KEY (train_id),
     FOREIGN KEY (transit_line_name) REFERENCES TransitLines(name)
-);
+) AUTO_INCREMENT = 1000;
 
 -- Train Schedule Data Tables
 CREATE TABLE Schedules (
@@ -51,29 +48,31 @@ CREATE TABLE TrainStops (
 
 -- User Data Tables
 CREATE TABLE Users (
-    user_id INT PRIMARY KEY,
     last_name VARCHAR(100) NOT NULL,
     first_name VARCHAR(100) NOT NULL,
-    username VARCHAR(100) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL
+    username VARCHAR(100) PRIMARY KEY,
+    password VARCHAR(255) NOT NULL,
+    email_address VARCHAR(100) UNIQUE,
+    social_security_number VARCHAR(11) UNIQUE
 );
 
-CREATE TABLE Customers (
-    customer_id INT PRIMARY KEY,
-    email_address VARCHAR(100) UNIQUE NOT NULL,
-    FOREIGN KEY (customer_id) REFERENCES Users(user_id)
-);
+-- If required later: have two tables (Coming Soon)
+-- CREATE TABLE Customers (
+--     customer_id INT PRIMARY KEY,
+--     email_address VARCHAR(100) UNIQUE NOT NULL,
+--     FOREIGN KEY (customer_id) REFERENCES Users(user_id)
+-- );
 
-CREATE TABLE Employees (
-    employee_id INT PRIMARY KEY,
-    social_security_number VARCHAR(11) UNIQUE NOT NULL,
-    FOREIGN KEY (employee_id) REFERENCES Users(user_id)
-);
+-- CREATE TABLE Employees (
+--     employee_id INT PRIMARY KEY,
+--     social_security_number VARCHAR(11) UNIQUE NOT NULL,
+--     FOREIGN KEY (employee_id) REFERENCES Users(user_id)
+-- );
 
 -- Reservation Data Tables
 CREATE TABLE Reservations (
     reservation_no INT PRIMARY KEY,
-    customer_id INT,
+    customer VARCHAR(100),
     reservation_date DATE NOT NULL,
     origin_station_id INT,
     destination_station_id INT,
@@ -81,36 +80,42 @@ CREATE TABLE Reservations (
     departure_datetime DATETIME NOT NULL,
     total_fare DECIMAL(10, 2) NOT NULL,
     is_round_trip BOOLEAN NOT NULL,
-    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
+    FOREIGN KEY (customer) REFERENCES Users(username),
     FOREIGN KEY (origin_station_id) REFERENCES Stations(station_id),
     FOREIGN KEY (destination_station_id) REFERENCES Stations(station_id),
     FOREIGN KEY (schedule_id) REFERENCES Schedules(schedule_id)
 );
 
--- Access Control Tables
+-- Access Control Tables (Coming Soon)
 CREATE TABLE UserRoles (
     user_role_id INT PRIMARY KEY,
-    user_id INT,
+    username VARCHAR(100),
     role_type ENUM('manager', 'customer_rep', 'customer') NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+    FOREIGN KEY (username) REFERENCES Users(username)
 );
 
--- Forum and Customer Service Tables
-CREATE TABLE Questions (
-    question_id INT PRIMARY KEY,
-    customer_id INT,
-    question_text TEXT NOT NULL,
-    timestamp DATETIME NOT NULL,
-    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
-);
+-- Forum and Customer Service Tables (Coming Soon)
+-- CREATE TABLE Questions (
+--     question_id INT PRIMARY KEY,
+--     customer_id INT,
+--     question_text TEXT NOT NULL,
+--     timestamp DATETIME NOT NULL,
+--     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+-- );
 
-CREATE TABLE Answers (
-    answer_id INT PRIMARY KEY,
-    question_id INT,
-    employee_id INT,
-    answer_text TEXT NOT NULL,
-    timestamp DATETIME NOT NULL,
-    FOREIGN KEY (question_id) REFERENCES Questions(question_id),
-    FOREIGN KEY (employee_id) REFERENCES Employees(employee_id)
-);
+-- CREATE TABLE Answers (
+--     answer_id INT PRIMARY KEY,
+--     question_id INT,
+--     employee_id INT,
+--     answer_text TEXT NOT NULL,
+--     timestamp DATETIME NOT NULL,
+--     FOREIGN KEY (question_id) REFERENCES Questions(question_id),
+--     FOREIGN KEY (employee_id) REFERENCES Employees(employee_id)
+-- );
+
+
+-- INSERT User data
+
+INSERT into Users (last_name, first_name, username, password, email_address)
+VALUES ('Bharadwaj', 'Abhinav', 'customer', 'password123', 'abhinav.bharadwaj@example.com');
 
